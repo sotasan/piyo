a230200e351f5924e60f40691e30bafa1f72e16b
Sōta <sota@sota.moe>
2026-05-10 23:31:41 +0200

    chore(docs): rename docs/docs to docs/src

--- docs/rspress.config.ts
+++ docs/rspress.config.ts
@@ -1,8 +1,9 @@
 import * as path from "node:path";
+
 import { defineConfig } from "@rspress/core";
 
 export default defineConfig({
-    root: path.join(__dirname, "docs"),
+    root: path.join(__dirname, "src"),
     title: "My Site",
     icon: "/rspress-icon.png",
     logo: {
