e278a845f5ebba90fd25f7904065fc29faf48dc4
Sōta <sota@sota.moe>
2026-05-12 21:44:18 +0000

    build(macos): share C prototypes via piyo.h for compile-time FFI checks

---
+++ src-tauri/src/macos/piyo.h
@@ -0,0 +1,15 @@
+#ifndef PIYO_MACOS_H
+#define PIYO_MACOS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void piyo_install_context_menu(void);
+void piyo_install_refresh_rate(void *wk_webview_ptr);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
